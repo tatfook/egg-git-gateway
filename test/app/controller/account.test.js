56f2493ad2de5f1014c3cb4aff43054b162dbc47
William <williamzjb@163.com>
2018-08-14 14:43:44 +0800

    Update tests of projects and accounts

--- test/app/controller/account.test.js
+++ test/app/controller/account.test.js
@@ -4,20 +4,33 @@ const { app } = require('egg-mock/bootstrap');
 
 describe('test/app/controller/account.test.js', () => {
 
+  const admin = {
+    username: 'unittest',
+    _id: 15,
+    roleId: 10,
+  };
+
   it('should post /accounts to create an account', () => {
     const user = {
       username: 'gitgateway123',
       password: '12345678',
     };
+
+    const secret = app.config.jwt.secret;
+    const token = app.jwt.sign(admin, secret);
     return app.httpRequest()
       .post('/accounts')
       .send(user)
+      .set('Authorization', `Bearer ${token}`)
       .expect(201);
   });
 
   it('should delete /accounts/:id to delete an account', () => {
+    const secret = app.config.jwt.secret;
+    const token = app.jwt.sign(admin, secret);
     return app.httpRequest()
       .del('/accounts/gitgateway123')
+      .set('Authorization', `Bearer ${token}`)
       .expect(204);
   });
 });
--- test/app/controller/project.test.js
+++ test/app/controller/project.test.js
@@ -10,18 +10,30 @@ describe('test/app/controller/project.test.js', () => {
     visibility: 'public',
   };
 
+  const admin = {
+    username: 'unittest',
+    _id: 15,
+    roleId: 10,
+  };
+
   it('should post /projects/user/:kw_username to create a project', () => {
+    const secret = app.config.jwt.secret;
+    const token = app.jwt.sign(admin, secret);
     return app.httpRequest()
       .post('/projects/user/backend')
+      .set('Authorization', `Bearer ${token}`)
       .send(project)
       .expect(201);
   });
 
   it('should put /projects/:path/visibility to update the visibility of a project', () => {
     const path = encodeURIComponent('backend/justtest1234');
+    const secret = app.config.jwt.secret;
+    const token = app.jwt.sign(admin, secret);
     return app.httpRequest()
       .put(`/projects/${path}/visibility`)
       .send({ visibility: 'private' })
+      .set('Authorization', `Bearer ${token}`)
       .expect(200)
       .expect(res => {
         assert(res.body.visibility === 'private');
@@ -30,8 +42,11 @@ describe('test/app/controller/project.test.js', () => {
 
   it('should delete /projects/:path to delete an project', () => {
     const path = encodeURIComponent('backend/justtest1234');
+    const secret = app.config.jwt.secret;
+    const token = app.jwt.sign(admin, secret);
     return app.httpRequest()
       .del(`/projects/${path}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(204);
   });
 });
