5a3295c86cfcae24ed97f0210e17691dbce47a0d
William <williamzjb@163.com>
2019-07-24 17:18:18 +0800

    Update tests of folders controller

--- test/app/controller/folder.test.js
+++ test/app/controller/folder.test.js
@@ -1,66 +1,73 @@
 'use strict';
 
-const { app } = require('egg-mock/bootstrap');
-const jwt = require('keepwork-jwt-simple');
+const { app, assert } = require('egg-mock/bootstrap');
+// const faker = require('faker');
 
+const FOLDER_TYPE = 'tree';
+
+let factory;
+let project;
+let account;
 let token;
-const project_path = encodeURIComponent('test_folder/test_folder');
+let encodedProjectPath;
+let option;
 
 before(async () => {
-  const admin = {
-    username: 'unittest',
-    userId: 15,
-    roleId: 10,
+  factory = app.factory;
+  project = await factory.create('Project');
+  account = await app.model.Account.findOne({ _id: project.account_id });
+  encodedProjectPath = encodeURIComponent(project.path);
+  token = app.mock.utils.token.get(account);
+  option = {
+    project_id: project._id,
+    account_id: account._id,
   };
-  const secret = app.config.jwt.secret;
-  token = jwt.encode(admin, secret, 'HS1');
-
-  const user = {
-    id: 123,
-    username: 'test_folder',
-    password: '12345678',
-  };
-
-  const project = {
-    sitename: 'test_folder',
-    site_id: 123,
-    visibility: 'public',
-  };
-
-  await app.httpRequest()
-    .post('/accounts')
-    .send(user)
-    .set('Authorization', `Bearer ${token}`);
-
-  await app.httpRequest()
-    .post('/projects/user/test_folder')
-    .set('Authorization', `Bearer ${token}`)
-    .send(project);
 });
 
-after(async () => {
-  await app.httpRequest()
-    .del(`/projects/${encodeURIComponent('test_folder/test_folder')}`)
-    .set('Authorization', `Bearer ${token}`);
-
-  await app.httpRequest()
-    .del('/accounts/test_folder')
-    .set('Authorization', `Bearer ${token}`);
+beforeEach(() => {
+  app.mock.axios.keepwork.set(200, 64);
 });
 
-const path = encodeURIComponent('test_folder/test_folder/new_folder');
 describe('test/app/controller/folder.test.js', () => {
-  it('should post /projects/:project_path/folders/:path to create a folder', () => {
-    return app.httpRequest()
-      .post(`/projects/${project_path}/folders/${path}`)
+  it('should post /projects/:project_path/folders/:path to create a folder', async () => {
+    const path = 'test_folder/test_folder/new_folder1';
+    const encodedFolderPath = encodeURIComponent(path);
+
+    await app.httpRequest()
+      .post(`/projects/${encodedProjectPath}/folders/${encodedFolderPath}`)
       .set('Authorization', `Bearer ${token}`)
       .expect(201);
+
+    const folderGetFromDB = await app.model.Node.findOne({ project_id: project._id, path });
+    assert(folderGetFromDB);
+    assert(folderGetFromDB.type === FOLDER_TYPE);
   });
 
-  it('should delete /projects/:project_path/folders/:path to remove a folder', () => {
-    return app.httpRequest()
-      .del(`/projects/${project_path}/folders/${path}`)
+  it('should delete /projects/:project_path/folders/:path to remove a folder', async () => {
+    const folder = await factory.create('Node', option, { isFolder: true });
+    const { path } = folder;
+    const encodedFolderPath = encodeURIComponent(path);
+
+    await app.httpRequest()
+      .del(`/projects/${encodedProjectPath}/folders/${encodedFolderPath}`)
       .set('Authorization', `Bearer ${token}`)
       .expect(200);
+
+    const folderGetFromDB = await app.model.Node.findOne({ project_id: project._id, path });
+    assert(folderGetFromDB === null);
+  });
+
+  it('should delete /projects/:project_path/folders/:path to remove a folder', async () => {
+    const folder = await factory.create('Node', option);
+    const { path } = folder;
+    const encodedFolderPath = encodeURIComponent(path);
+
+    await app.httpRequest()
+      .del(`/projects/${encodedProjectPath}/folders/${encodedFolderPath}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    const folderGetFromDB = await app.model.Node.findOne({ project_id: project._id, path });
+    assert(folderGetFromDB === null);
   });
 });
--- test/mock/factory/node.js
+++ test/mock/factory/node.js
@@ -2,12 +2,29 @@
 
 const faker = require('faker');
 
+const FILE_TYPE = 'blob';
+const FOLDER_TYPE = 'tree';
+
 module.exports = app => {
   const { factory } = app;
-  factory.define('Node', app.model.Node, async () => {
+  factory.define('Node', app.model.Node, async options => {
+    let name;
+    let path;
+    let type;
+    let content;
+
     const project = await factory.create('Project');
-    const name = faker.system.fileName();
-    const path = `${faker.lorem.word()}/${faker.lorem.word()}/${name}`;
+
+    if (options.isFolder) {
+      name = faker.lorem.word();
+      path = `${faker.lorem.word()}/${name}`;
+      type = FOLDER_TYPE;
+    } else {
+      name = faker.system.fileName();
+      path = `${faker.lorem.word()}/${faker.lorem.word()}/${name}`;
+      content = faker.lorem.paragraphs();
+      type = FILE_TYPE;
+    }
 
     const commit_id = faker.random.uuid().split('-').join('');
     const short_id = commit_id.slice(0, 9);
@@ -22,13 +39,13 @@ module.exports = app => {
 
     const attrs = {
       name, path,
-      content: faker.lorem.paragraphs(),
-      type: 'blob',
+      content, type,
       project_id: project._id,
       account_id: project.account_id,
       commits: [ commit ],
       latest_commit: commit,
     };
+
     return attrs;
   });
 };
